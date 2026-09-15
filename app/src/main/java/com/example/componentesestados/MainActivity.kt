f09f6428fb55de8c3a7cbb0ffd51b1a1f716a781
MATHEUS HENRY DOS SANTOS <25204051@dsn127m1242373.cfp127aluno.local>
2026-09-15 16:57:21 -0300

    Subindo o que fiz até agora, para da continuidade depois

---
+++ app/src/main/java/com/example/componentesestados/MainActivity.kt
@@ -0,0 +1,62 @@
+package com.example.componentesestados
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import com.example.componentesestados.ui.theme.ComponentesEstadosTheme
+import com.example.componentesestados.ui.theme.Righteous
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ComponentesEstadosTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    BasicComponentsScreen(
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun BasicComponentsScreen(modifier: Modifier = Modifier) {
+    Column (
+        modifier = modifier.fillMaxSize()
+            .background(Color(239, 247, 207))
+    ) {
+        Text(
+            text = "Aula android",
+            fontSize = 32.sp,
+            fontWeight = FontWeight.Bold,
+            color = Color(0xFF3DDC84),
+            fontFamily = FontFamily.Serif
+        )
+        Text(
+            text = "com Jetpack Compose",
+            fontSize = 16.sp,
+            fontWeight = FontWeight.SemiBold,
+            color = Color(66, 133, 244),
+            fontFamily = Righteous
+        )
+    }
+}
