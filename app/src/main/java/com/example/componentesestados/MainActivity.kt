4891b3bcda73fd8792d09c2f8e14777cf78daa4c
MATHEUS HENRY DOS SANTOS <25204051@dsn127m1242373.cfp127aluno.local>
2026-09-24 16:43:29 -0300

    Criação de novos componentes e seus estados

--- app/src/main/java/com/example/componentesestados/MainActivity.kt
+++ app/src/main/java/com/example/componentesestados/MainActivity.kt
@@ -2,24 +2,48 @@ package com.example.componentesestados
 
 import android.os.Bundle
 import android.util.Log
+import android.util.Log.i
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.FavoriteBorder
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.filled.Star
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.CheckboxDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
+import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -30,7 +54,6 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardCapitalization
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.componentesestados.ui.theme.ComponentesEstadosTheme
@@ -54,17 +77,45 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun BasicComponentsScreen(modifier: Modifier = Modifier) {
-    val textInput = remember {
+    var textInput by remember {
         mutableStateOf("")
     }
 
-    val quantidade = remember {
+    var quantidade by remember {
         mutableStateOf("")
+    }
+
+    var email by remember {
+        mutableStateOf("")
+    }
+
+    var kotlin by remember {
+        mutableStateOf(false)
+    }
+
+    var iComposable by remember {
+        mutableStateOf(false)
+    }
+
+    var android by remember {
+        mutableStateOf(false)
+    }
+
+    var sistemaSelecionado by remember {
+        mutableStateOf(0)
+    }
+
+    var corFundo by remember {
+        mutableStateOf(Color(239, 247, 207))
+    }
+
+    var favoritado by remember {
+        mutableStateOf(false)
     }
 
     Column (
         modifier = modifier.fillMaxSize()
-            .background(Color(239, 247, 207))
+            .background(corFundo)
     ) {
         Text(
             text = "Aulas android",
@@ -76,6 +127,7 @@ fun BasicComponentsScreen(modifier: Modifier = Modifier) {
             textAlign = TextAlign.End,
             letterSpacing = 4.sp
         )
+
         Text(
             text = "com Jetpack Compose",
             fontSize = 16.sp,
@@ -92,35 +144,230 @@ fun BasicComponentsScreen(modifier: Modifier = Modifier) {
 
         TextField(
             modifier = Modifier.fillMaxWidth(),
-            value = textInput.value,
+            value = textInput,
             onValueChange = { novoValor ->
                 Log.i("teste", novoValor)
 
-                textInput.value = novoValor
+                textInput = novoValor
             },
             keyboardOptions = KeyboardOptions(
                 capitalization = KeyboardCapitalization.Words
             ),
             label = {
                 Text("Nome e sobrenome")
+            },
+            leadingIcon = {
+                Icon(
+                    imageVector = Icons.Default.Person,
+                    contentDescription = "Pessoa",
+                    tint = Color(66, 133, 244)
+                )
+            },
+            trailingIcon = {
+                Icon(
+                    imageVector = Icons.Default.Person,
+                    contentDescription = "Ícone Pessoa",
+                    tint = Color(66, 133, 244)
+                )
             }
         )
 
         TextField(
             modifier = Modifier.fillMaxWidth(),
-            value = quantidade.value,
+            value = quantidade,
             onValueChange = { novoValor ->
                 Log.i("teste", novoValor)
 
-                quantidade.value = novoValor
+                quantidade = novoValor
             },
             keyboardOptions = KeyboardOptions(
                 keyboardType = KeyboardType.Number
             ),
             placeholder = {
                 Text("Qual a quantidade?")
-            }
+            },
+            colors = TextFieldDefaults.colors(
+                focusedTextColor = Color.Blue,
+                unfocusedTextColor = Color.Red,
+                unfocusedPlaceholderColor = Color.Magenta
+            )
         )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        OutlinedTextField(
+            value = email,
+            onValueChange = { email = it },
+            singleLine = true,
+            modifier = Modifier.fillMaxWidth(),
+            label = {
+                Text( text = "Email" )
+            },
+            placeholder = {
+                Text( text = "Digite seu email" )
+            },
+            shape = RoundedCornerShape(
+                topStart = 32.dp,
+                bottomEnd = 32.dp
+            ),
+            colors = OutlinedTextFieldDefaults.colors(
+                focusedBorderColor = Color.Gray,
+                unfocusedBorderColor = Color.Blue
+            )
+        )
+
+        Row(
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Checkbox(
+                checked = kotlin,
+                onCheckedChange = { kotlin = it },
+                colors = CheckboxDefaults.colors(
+                    checkedColor = Color.Blue,
+                    uncheckedColor = Color.Red
+                )
+            )
+
+            Text(
+                text = "Kotlin"
+            )
+        }
+
+        Row(
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Checkbox(
+                checked = iComposable,
+                onCheckedChange = { iComposable = it },
+                colors = CheckboxDefaults.colors(
+                    checkedColor = Color.Blue,
+                    uncheckedColor = Color.Red
+                )
+            )
+
+            Text(
+                text = "Jatpack Compose"
+            )
+        }
+
+        Row(
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Checkbox(
+                checked = android,
+                onCheckedChange = { android = it },
+                colors = CheckboxDefaults.colors(
+                    checkedColor = Color.Blue,
+                    uncheckedColor = Color.Red
+                )
+            )
+
+            Text(
+                text = "Android"
+            )
+        }
+
+        Row(
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            RadioButton(
+                selected = sistemaSelecionado == 0,
+                onClick = { sistemaSelecionado = 0 }
+            )
+
+            Text(
+                text = "MacOS"
+            )
+        }
+
+        Row(
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            RadioButton(
+                selected = sistemaSelecionado == 1,
+                onClick = { sistemaSelecionado = 1 }
+            )
+
+            Text(
+                text = "Windows"
+            )
+        }
+
+        Row(
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            RadioButton(
+                selected = sistemaSelecionado == 2,
+                onClick = { sistemaSelecionado = 2 }
+            )
+
+            Text(
+                text = "Parrot Linux"
+            )
+        }
+
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceAround
+        ) {
+            Button(
+                onClick = {
+                    corFundo = Color.Cyan
+                },
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Color.Gray,
+                    contentColor = Color.Green
+                ),
+                border = BorderStroke(4.dp, Color.Red),
+                shape = RoundedCornerShape(topEnd = 12.dp, bottomStart = 12.dp)
+            ) {
+                Row(
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Icon(
+                        imageVector = Icons.Default.Star,
+                        contentDescription = ""
+                    )
+
+                    Text(
+                        "Clique aqui!"
+                    )
+                }
+            }
+
+            OutlinedButton(
+                onClick = {
+                    corFundo = Color.Red
+                }
+            ) {
+                Text("Outro Botão")
+            }
+        }
+
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.Center
+        ) {
+            if (favoritado) {
+                Icon(
+                    modifier = Modifier.size(40.dp)
+                        .clickable{
+                            favoritado = false
+                        },
+                    imageVector = Icons.Default.Favorite,
+                    contentDescription = "Favorite Icon"
+                )
+            } else {
+                Icon(
+                    modifier = Modifier.size(40.dp)
+                        .clickable{
+                            favoritado = true
+                        },
+                    imageVector = Icons.Default.FavoriteBorder,
+                    contentDescription = "Favorite Border Icon"
+                )
+            }
+        }
     }
 }
 
